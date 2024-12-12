b72c5065f38db4341e4f88dc7d705f36974dc406
nayrariosaguilar <nayrios2004@gmail.com>
2024-12-12 20:05:31 +0100

    everytinh up to date

--- composeApp/src/desktopMain/kotlin/com/nayx34/App.kt
+++ composeApp/src/desktopMain/kotlin/com/nayx34/App.kt
@@ -4,22 +4,50 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import app.cash.sqldelight.db.SqlDriver
+import com.nayx34.data.Database
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import nayrarios2uwu.composeapp.generated.resources.Res
 import nayrarios2uwu.composeapp.generated.resources.compose_multiplatform
+import javax.xml.crypto.Data
 
+object DatabaseConfig {
+    val name: String = "pets.db"
+    val development: Boolean = true
+}
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-       Text("naysita")
+
+fun App(sqlDriver: SqlDriver) {
+    val database = Database(sqlDriver)
+    Database.Schema.create(sqlDriver)
+
+    if(DatabaseConfig.development){
+        insertDatabase(database)
     }
+
+    MaterialTheme {
+        CarListScreen(database)
+    }
+}
+fun insertDatabase(db: Database){
+    db.carQueries.insertCar("azul")
+    db.carQueries.insertCar("verde")
+
+}
+
+@Composable
+fun CarListScreen(db: Database){
+    val cars = db.carQueries.selectCar().executeAsList()
+    LazyColumn { items(cars){ car -> Text(car.color)} }
 }
--- composeApp/src/desktopMain/kotlin/com/nayx34/main.kt
+++ composeApp/src/desktopMain/kotlin/com/nayx34/main.kt
@@ -2,12 +2,22 @@ package com.nayx34
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import javax.swing.DesktopManager
+import kotlin.io.path.Path
+import kotlin.io.path.deleteIfExists
 
 fun main() = application {
+
+
+    if (DatabaseConfig.development) {
+        Path(DatabaseConfig.name).deleteIfExists()
+    }
+    val driver = JdbcSqliteDriver("jdbc:sqlite:${DatabaseConfig.name}")
     Window(
         onCloseRequest = ::exitApplication,
         title = "NayraRios2uwu",
     ) {
-        App()
+        App(driver)
     }
 }
